d74f9bf749c3a025e7e2bccbfb56f79c5350875d
aurimas <auris.narkus@gmail.com>
2020-02-20 16:16:00 +0200

    Changed docker start file

--- docker/start.sh
+++ docker/start.sh
@@ -7,11 +7,11 @@ sed -i -E "s/TO_BE_REPLACED_WITH_PORT/${PORT:-8080}/" /etc/nginx/conf.d/*.conf
 mkdir /var/tmp/nginx
 
 # make php-fpm be able to listen request from nginx (current user is nginx executor)
-sed -i -E "s/^;listen.owner = .*/listen.owner = nonroot/" /etc/php7/php-fpm.d/www.conf
+sed -i -E "s/^;listen.owner = .*/listen.owner = $(whoami)/" /etc/php7/php-fpm.d/www.conf
 
 # make current user the executor of nginx and php-fpm expressly for local environment
-#sed -i -E "s/^user = .*/user = nonroot/" /etc/php7/php-fpm.d/www.conf
-#sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
-sed -i -E "s/^user .*/user nonroot;/" /etc/nginx/nginx.conf
+sed -i -E "s/^user = .*/user = $(whoami)/" /etc/php7/php-fpm.d/www.conf
+sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
+sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
