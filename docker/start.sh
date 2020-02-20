5af023672f69e14dfd0e98a97cc73fa54d33c4b7
aurimas <auris.narkus@gmail.com>
2020-02-20 15:50:00 +0200

    Removed php-fpm user directive

--- docker/start.sh
+++ docker/start.sh
@@ -10,8 +10,8 @@ mkdir /var/tmp/nginx
 sed -i -E "s/^;listen.owner = .*/listen.owner = nonroot/" /etc/php7/php-fpm.d/www.conf
 
 # make current user the executor of nginx and php-fpm expressly for local environment
-sed -i -E "s/^user = .*/user = nonroot/" /etc/php7/php-fpm.d/www.conf
-sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
+#sed -i -E "s/^user = .*/user = nonroot/" /etc/php7/php-fpm.d/www.conf
+#sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
 sed -i -E "s/^user .*/user nonroot;/" /etc/nginx/nginx.conf
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
